56f55f1498ccd1ccf9b0488abd63a2d10a32fba5
fathiara <fathia.alghifari@gmail.com>
2024-11-01 09:44:58 +0700

    Add files via upload
    tugas praktikum 5 ddp fathia

---
+++ Ddp_pekan 5.py
@@ -0,0 +1,30 @@
+my_list = ['motor',1, 'mobil','Toyota']
+my_list.append('suzuki')
+my_list.remove(1)
+my_list.insert(2,'Honda')
+print(my_list)
+
+my_list = [1,2,3,4]
+my_poplist =my_list.pop(3)
+print(my_list)
+print(my_poplist)
+
+
+my_tuple = (1,2,3,4)
+print(my_tuple[1])
+
+my_tuple[3] = 2
+print(my_tuple)
+
+#dictionary
+
+my_dictionary = {
+    'nama' : 'fathia',
+    'nim'  : 0110224095, 
+    'rombel' : 'TI10'
+     }
+print(my_dictionary)
+print(my_dictionary['rombel'])
+print(type('nim'))
+print(type(my_dictionary))
+
