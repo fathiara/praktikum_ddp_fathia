e1ed37e6d28c82013e9e089c08ef63991c097637
fathiara <fathia.alghifari@gmail.com>
2024-11-03 23:56:32 +0700

    Add files via upload
    ddp fathia pekan 6

---
+++ ddp_fathiapekan6_no2.py
@@ -0,0 +1,8 @@
+jumlah_total_bil_ganjil = 0
+bilangan_ganjil = 1
+
+while bilangan_ganjil <= 20:
+  jumlah_total_bil_ganjil += bilangan_ganjil
+  bilangan_ganjil += 2
+  print("1 + 3 + 5 + 7 + 9 + 11 + 13 + 15 + 17 + 19=",jumlah_total_bil_ganjil)
+  print("")
